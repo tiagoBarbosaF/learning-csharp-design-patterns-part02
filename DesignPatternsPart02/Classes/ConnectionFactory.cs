3921f6f716034e8862fc692a0dd7b93a441a438b
Tiago Barbosa <tiago@farias.cc>
2023-09-04 19:50:56 -0300

    factory pattern.

---
+++ DesignPatternsPart02/Classes/ConnectionFactory.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace DesignPatternsPart02.Classes;
+
+public class ConnectionFactory
+{
+    public IDbConnection GetConnection()
+    {
+        IDbConnection connection = new SqlConnection();
+
+        connection.ConnectionString = "User=root;Password=root;Server=localhost;Database=myDb";
+
+        connection.Open();
+
+        return connection;
+    }
+}
---
+++ DesignPatternsPart02/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesignPatternsPart02.Classes;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var connection = new ConnectionFactory().GetConnection();
+
+        var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM PERSON";
+    }
+}
+
